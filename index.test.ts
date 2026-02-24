93db919e902f475e2356e00ea42d9be7ad1203c9
Jack Ellison <64220909+tgr-wjya@users.noreply.github.com>
2026-02-25 03:29:45 +0700

    feat: update README with new dates and time spent; add index2.ts for task and echo API endpoints

--- index.test.ts
+++ index.test.ts
@@ -1,37 +1,123 @@
 /**
- * Elysia + Bun Test Runner
+ * Reformatting my ELysia + Bun test runner.
  *
  * @author Tegar Wijaya Kusuma
- * @date 18 - 24 February 2026
+ * @date 18 - 25 February 2026
  */
 
-import { describe, expect, it, test, spyOn } from 'bun:test';
-import { app } from './index';
-
-// TODO: REFACTOR ALL USING `DESCRIBE` AND `IT`
+import { describe, expect, it, spyOn } from 'bun:test';
+import { app } from './index2';
 
 const BASE_URL = 'http://localhost:3000';
 
-describe('Testing /root and /greet responsiveness', () => {
-  describe('ALL /root', () => {
-    it('Should return kaomoji at /root for ALL method', async () => {
-      const response = await app.handle(new Request(`${BASE_URL}`));
+type Task = {
+  id: number;
+  description: string;
+  status: string;
+};
 
-      const kaomoji = await response.text();
-      expect(kaomoji).toBe('made with ◉‿◉');
-    });
-  });
-
-  describe('GET /greet', () => {
-    it('Should return a greeting', async () => {
+// For /root and wildcards
+describe('Testing server responsiveness and wildcard', () => {
+  describe('GET /root', () => {
+    it('Should return a kaomoji and author (me)', async () => {
       const response = await app.handle(
-        new Request(`${BASE_URL}/greet`, {
+        new Request(`${BASE_URL}`, {
           method: 'GET',
         })
       );
 
-      const greeting = await response.json();
-      expect(greeting).toEqual({ greet: 'hello, world' });
+      const authorAndKaomoji = await response.json();
+      expect(authorAndKaomoji).toBeObject();
+      expect(authorAndKaomoji).toEqual({
+        kaomoji: 'made with ◉‿◉',
+        author: 'Tegar Wijaya Kusuma',
+      });
     });
+  });
+
+  describe('GET /* wildcards', () => {
+    it('Should returns an array of object', async () => {
+      const response = await app.handle(
+        new Request(`${BASE_URL}/test`, {
+          method: 'GET',
+          headers: { 'Content-Type': '	application/json' },
+        })
+      );
+
+      const wildcard = await response.json();
+      expect(wildcard).toBeObject();
+      expect(response.status).toBe(404);
+      expect(wildcard).toEqual({
+        error: 'Not found ¯\\_(ツ)_/¯',
+        message: "This endpoint doesn't exist",
+        availableEndpoints: [
+          'GET /',
+          'GET /tasks/all',
+          'GET /tasks/:id',
+          'POST /tasks',
+          'PATCH /tasks/:id',
+          'DELETE /tasks/:id',
+        ],
+      });
+    });
+  });
+});
+
+// echo
+describe('Testing /echo', () => {
+  it('Should echo user body', async () => {
+    const echoUser = {
+      username: 'Jack',
+      age: 22,
+    };
+
+    const response = await app.handle(
+      new Request(`${BASE_URL}/echo`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(echoUser),
+      })
+    );
+
+    expect(response.status).toBe(201);
+
+    const created = await response.json();
+    expect(created).toEqual({
+      username: 'Jack',
+      age: 22,
+    });
+  });
+});
+
+// taskGroup
+describe('Testing /tasks API', () => {
+  describe('GET /tasks/all', () => {
+    it('Should returns all tasks', async () => {
+      spyOn(Bun, 'file').mockReturnValue({
+        // eslint-disable-next-line @typescript-eslint/require-await
+        json: async () => [
+          { id: 71823891, description: 'what to put?', status: 'pending' },
+          { id: 67182912, description: 'hello, world!', status: 'completed' },
+        ],
+        // eslint-disable-next-line @typescript-eslint/require-await
+        exists: async () => true,
+      } as unknown as ReturnType<typeof Bun.file>);
+
+      const response = await app.handle(
+        new Request(`${BASE_URL}/tasks/all`, {
+          method: 'GET',
+        })
+      );
+
+      const tasks = (await response.json()) as Task[];
+      expect(response.status).toBe(200);
+      expect(Array.isArray(tasks)).toBe(true);
+      expect(tasks[0]).toHaveProperty('id');
+    });
+  });
+
+  describe('POST /tasks', () => {
+    it('Should send and return POSTED task', async () => {});
+    // TODO: Continue this tomorrow.
   });
 });
